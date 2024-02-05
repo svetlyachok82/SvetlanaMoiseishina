7ca5b790384a552daf0d5313223330bf2aa1bcc0
svetlyachok82 <sveta.moiseishina@gmail.com>
2024-02-05 23:27:10 +0300

    Hometask7

---
+++ Hometask7/MyArraySizeException.java
@@ -0,0 +1,9 @@
+
+package Task7;
+import java.util.Arrays;
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+}
